26cb3a0bf52d35dcbd61a4385c1d43d9339de990
Ajay Gaur <gaurajay787@gmail.com>
2026-02-24 00:01:03 +0530

    d

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("CI/CD Working");
 });
+app.use("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen(3000, () => console.log("Server running"));
